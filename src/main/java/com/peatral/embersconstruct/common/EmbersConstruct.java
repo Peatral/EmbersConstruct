8ba5c6a79f92a7fcb458818bdf6b854929ffef77
Christof <christof.reimers@outlook.de>
2020-04-07 13:01:03 +0200

    update version

--- src/main/java/com/peatral/embersconstruct/common/EmbersConstruct.java
+++ src/main/java/com/peatral/embersconstruct/common/EmbersConstruct.java
@@ -33,7 +33,7 @@ public class EmbersConstruct {
     public static final String MODID = "embersconstruct";
     public static final String MOD_NAME = "Embers' Construct";
     public static final String NAME = "Embers Construct";
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "1.2.1";
     public static final String DEPENDENCIES =
             "required-after:tconstruct;" +
             "required-after:embers;" +
