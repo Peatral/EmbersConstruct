9a8e25a292dfe6d8d12cb4fcbf4aa72e1f14c545
Christof <christof.reimers@outlook.de>
2021-09-29 10:52:29 +0200

    Fix ConArm armor traits
    Using tool traits for armor is bullshit lol (but funny)

--- src/main/java/com/peatral/embersconstruct/integration/conarm/IntegrationConarm.java
+++ src/main/java/com/peatral/embersconstruct/integration/conarm/IntegrationConarm.java
@@ -1,17 +1,23 @@
 package com.peatral.embersconstruct.integration.conarm;
 
 import c4.conarm.common.ConstructsRegistry;
+import c4.conarm.common.armor.traits.ArmorTraits;
+import c4.conarm.lib.materials.ArmorMaterialType;
 import c4.conarm.lib.materials.CoreMaterialStats;
 import c4.conarm.lib.materials.PlatesMaterialStats;
 import c4.conarm.lib.materials.TrimMaterialStats;
+import com.google.common.eventbus.Subscribe;
 import com.peatral.embersconstruct.EmbersConstructMaterials;
 import com.peatral.embersconstruct.modules.EmbersConstructModule;
 import com.peatral.embersconstruct.util.Stamp;
-import net.minecraft.item.Item;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import slimeknights.mantle.pulsar.pulse.Pulse;
 import slimeknights.tconstruct.library.TinkerRegistry;
+import slimeknights.tconstruct.library.materials.Material;
+import slimeknights.tconstruct.library.traits.ITrait;
 
 @Pulse(id = IntegrationConarm.PulseId,
         modsRequired = IntegrationConarm.modid,
@@ -32,11 +38,32 @@ public class IntegrationConarm extends EmbersConstructModule {
                 initStamp("leggings_core", ConstructsRegistry.leggingsCore));
     }
 
-    @SubscribeEvent
-    public void registerItems(RegistryEvent.Register<Item> event) {
+    @Subscribe
+    public void preInit(FMLPreInitializationEvent event) {
         TinkerRegistry.addMaterialStats(EmbersConstructMaterials.wroughtiron,
                 new CoreMaterialStats(9, 15),
                 new PlatesMaterialStats(0.85F, 3, 0),
                 new TrimMaterialStats(2.5F));
+    }
+
+    @Subscribe
+    public void init(FMLInitializationEvent event){
+        addArmorTrait(EmbersConstructMaterials.wroughtiron, ArmorTraits.magnetic2, (ITrait)ArmorTraits.magnetic);
+    }
+
+    public static void addArmorTrait(Material material, ITrait trait, ITrait secondTrait) {
+        material.addTrait(trait, ArmorMaterialType.CORE);
+        if (secondTrait != null) {
+            material.addTrait(secondTrait, ArmorMaterialType.PLATES);
+            material.addTrait(secondTrait, ArmorMaterialType.TRIM);
+        } else {
+            material.addTrait(trait, ArmorMaterialType.PLATES);
+            material.addTrait(trait, ArmorMaterialType.TRIM);
+        }
+
+    }
+
+    public static void addArmorTrait(Material material, ITrait trait, String type) {
+        material.addTrait(trait, type);
     }
 }
